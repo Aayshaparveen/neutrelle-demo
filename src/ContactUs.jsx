e1aa8bad08226f468bf645114b3773c65b68777c
Aayshaparveen <aaysha.parveen@ismartapps.co.uk>
2023-02-08 12:13:02 +0530

    Add Contact form & Add some Css

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -1,29 +1,102 @@
 import React from "react";
 import "./ConactUsStyle.css";
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import FormGroup from "@mui/material/FormGroup";
+import styled from "@emotion/styled";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+const CssTextField = styled(TextField)({
+  "& label.Mui-focused": {
+    color: "gray",
+    background: "white",
+    padding: "0px 10px",
+  },
+
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      boxShadow: "0px 0px 1px 0px",
+      borderRadius: "1px 15px",
+      border: "1px solid rgb(226, 226, 226) ",
+    },
+    "&:hover fieldset": {
+      borderColor: "transparent",
+    },
+    "&.Mui-focused fieldset": {
+      // borderColor: 'black',
+      border: "1px solid black",
+      borderRadius: "1px 15px",
+    },
+  },
+});
+<Box
+  sx={{
+    display: "flex",
+    alignItems: "center",
+
+    "& > :not(style)": { m: 1 },
+  }}
+/>;
 const ContactUs = () => {
   return (
+    <>
     <div className="Container">
       <div className="contact-box">
         <div>
           <span className="contact-heading">Contact Us</span>
         </div>
-        <p class="We-are-here-to-help">
+        <p className="We-are-here-to-help">
           We are here to help. Please enter your message and a member of the
           team will be in touch within 48 hours!
         </p>
         <form>
-        <TextField id="demo-helper-text-misaligned-no-helper1" label=" Your Name *" placeholder="Full Name" />
-        <TextField id="demo-helper-text-misaligned-no-helper2" label=" Email *" placeholder="Your Email" />
-        <TextField id="demo-helper-text-misaligned-no-helper3" label=" Message *" placeholder="Your Message" />
+          <CssTextField
+            label="Your Name *"
+            placeholder="Full Name"
+            id="custom-css-outlined-input"
+          />
+          <CssTextField
+            label="Email *"
+            placeholder="Your Email"
+            id="custom-css-outlined-input"
+          />
+          <TextField
+            placeholder="Your Message"
+            id="demo-helper-text-aligned"
+            label=" Message"
+          />
         </form>
+        <div className="checkbox">
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="I'm not a robot"
+            />
+          </FormGroup>
+        </div>
+        <div>
+          <button className="Message-box">Send Message</button>
+        </div>
       </div>
 
       <div className="contact-img-box">
         <img src="./image\illustration-contact-us-1.png"></img>
       </div>
+
     </div>
+      <div className="last-impact">
+        <div className="section">
+          <div className="box1">
+            <h2>Let's decarbonise our World!</h2>
+          </div>
+          <div className="box2">
+            <button className="impact-button">
+              <a href="./ImpactCalculator">Make your Impact</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
