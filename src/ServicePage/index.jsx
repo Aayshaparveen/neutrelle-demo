468147531d4773f1780a17ffc38dec101482dfe1
Aayshaparveen <aaysha.parveen@ismartapps.co.uk>
2023-02-18 00:32:47 +0530

    Add Code and Css in ServicePage

--- src/ServicePage/index.jsx
+++ src/ServicePage/index.jsx
@@ -44,88 +44,230 @@ const Service = () => {
         </div>
         <div className="Form-col">
           <div className="col-1">
-            <CssTextField label="Title" id="custom-css-outlined-input" />
-            <FormControl sx={{ m: 1, minWidth: 120 }}>
-              <Select
-                value={age}
-                // onChange={handleChange}
-                displayEmpty
-                inputProps={{ "aria-label": "Without label" }}
-              >
-                <MenuItem value="">
-                  <em>Category</em>
-                </MenuItem>
-                <MenuItem value={1}>Category1</MenuItem>
-                <MenuItem value={2}>Category2</MenuItem>
-                <MenuItem value={3}>Category3</MenuItem>
-                <MenuItem value={3}>Category4</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl sx={{ m: 1, minWidth: 120 }}>
-              <Select
-                value={age}
-                displayEmpty
-                inputProps={{ "aria-label": "Without label" }}
-              >
-                <MenuItem value="">
-                  <em>Primary Point of Contact</em>
-                </MenuItem>
-                <MenuItem value={1}>Category1</MenuItem>
-                <MenuItem value={2}>Category2</MenuItem>
-                <MenuItem value={3}>Category3</MenuItem>
-                <MenuItem value={3}>Category4</MenuItem>
-              </Select>
-            </FormControl>
+            <div className="Title">
+              <label>
+                Title
+                <input type="text" className="Title-box" />
+              </label>
+            </div>
+            {/* <CssTextField  id="custom-css-outlined-input" /> */}
+            <div className="Category">
+              <label>Category</label>
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <Select
+                  value={age}
+                  displayEmpty
+                  inputProps={{ "aria-label": "Without label" }}
+                >
+                  <MenuItem value="">
+                    <em></em>
+                  </MenuItem>
+                  <MenuItem value={1}>Category1</MenuItem>
+                  <MenuItem value={2}>Category2</MenuItem>
+                  <MenuItem value={3}>Category3</MenuItem>
+                  <MenuItem value={4}>Category4</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <div className="Primary">
+              <label>Primary Point of Contact</label>
+
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <Select
+                  value={age}
+                  displayEmpty
+                  inputProps={{ "aria-label": "Without label" }}
+                >
+                  <MenuItem value="">
+                    <em></em>
+                  </MenuItem>
+                  <MenuItem value={1}>Category1</MenuItem>
+                  <MenuItem value={2}>Category2</MenuItem>
+                  <MenuItem value={3}>Category3</MenuItem>
+                  <MenuItem value={3}>Category4</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
           </div>
           <div className="col-2">
-          <FormControl sx={{ m: 1, minWidth: 120 }}>
-              <Select
-                value={age}
-              
-                displayEmpty
-                inputProps={{ "aria-label": "Without label" }}
-              >
-                <MenuItem value="">
-                  <em>Queue</em>
-                </MenuItem>
-                <MenuItem value={1}>Queue1</MenuItem>
-                <MenuItem value={2}>Queue2</MenuItem>
-                <MenuItem value={3}>Queue3</MenuItem>
-                <MenuItem value={3}>Queue4</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl sx={{ m: 1, minWidth: 120 }}>
-              <Select
-                value={age}
-              
-                displayEmpty
-                inputProps={{ "aria-label": "Without label" }}
-              >
-                <MenuItem value="">
-                  <em>Assigned to</em>
-                </MenuItem>
-                <MenuItem value={1}>Queue1</MenuItem>
-                <MenuItem value={2}>Queue2</MenuItem>
-                <MenuItem value={3}>Queue3</MenuItem>
-                <MenuItem value={3}>Queue4</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl sx={{ m: 1, minWidth: 120 }}>
-              <Select
-                value={age}
-              
-                displayEmpty
-                inputProps={{ "aria-label": "Without label" }}
-              >
-                <MenuItem value="">
-                  <em>Channel Type</em>
-                </MenuItem>
-                <MenuItem value={1}>Queue1</MenuItem>
-                <MenuItem value={2}>Queue2</MenuItem>
-                <MenuItem value={3}>Queue3</MenuItem>
-                <MenuItem value={3}>Queue4</MenuItem>
-              </Select>
-            </FormControl>
+            <div className="Queue">
+              <label>Queue</label>
+
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <Select
+                  value={age}
+                  displayEmpty
+                  inputProps={{ "aria-label": "Without label" }}
+                >
+                  <MenuItem value="">
+                    <em></em>
+                  </MenuItem>
+                  <MenuItem value={1}>Queue1</MenuItem>
+                  <MenuItem value={2}>Queue2</MenuItem>
+                  <MenuItem value={3}>Queue3</MenuItem>
+                  <MenuItem value={3}>Queue4</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <div className="Assigned">
+              <label>Assigned to</label>
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <Select
+                  value={age}
+                  displayEmpty
+                  inputProps={{ "aria-label": "Without label" }}
+                >
+                  <MenuItem value="">
+                    <em></em>
+                  </MenuItem>
+                  <MenuItem value={1}>Queue1</MenuItem>
+                  <MenuItem value={2}>Queue2</MenuItem>
+                  <MenuItem value={3}>Queue3</MenuItem>
+                  <MenuItem value={3}>Queue4</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <div className="Channel">
+              <label>Channel Type</label>
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <Select
+                  value={age}
+                  displayEmpty
+                  inputProps={{ "aria-label": "Without label" }}
+                >
+                  <MenuItem value="">
+                    <em></em>
+                  </MenuItem>
+                  <MenuItem value={1}>Queue1</MenuItem>
+                  <MenuItem value={2}>Queue2</MenuItem>
+                  <MenuItem value={3}>Queue3</MenuItem>
+                  <MenuItem value={3}>Queue4</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+          </div>
+        </div>
+        <div className="Nature-box">
+          <div className="Nature-heading">
+            Nature of Allegation / Case Summary
+          </div>
+          <div className="Two-colum">
+            <div className="Nature-col-1">
+              <div className="Summary">
+                <label>Summary of Allegation</label>
+                <input type="text" />
+              </div>
+              <div className="Case">
+                <label>
+                  Nature of Case
+                  <select>
+                    <option value="">Other</option>
+                    <option value="">Case1</option>
+                    <option value="">Case2</option>
+                    <option value="">Case3</option>
+                    <option value="">Case4</option>
+                  </select>
+                </label>
+              </div>
+              <div className="Suspension">
+                <label>Suspension Date</label>
+                <input type="date" />
+              </div>
+              <div className="Return">
+                <label>Suspension Return Date </label>
+                <input type="date" />
+              </div>
+              <div className="Characteristics">
+                <label>
+                  Profected Characteristics
+                  <select>
+                    <option value="">Characteristics1</option>
+                    <option value="">Characteristics2</option>
+                    <option value="">Characteristics3</option>
+                    <option value="">Characteristics4</option>
+                    <option value="">Characteristics5</option>
+                  </select>
+                </label>
+              </div>
+              <div className="Respondent">
+                <label>
+                  Respondent
+                  <select>
+                    <option value=""> Respondent1</option>
+                    <option value=""> Respondent2</option>
+                    <option value=""> Respondent3</option>
+                    <option value=""> Respondent4</option>
+                    <option value=""> Respondent5</option>
+                  </select>
+                </label>
+              </div>
+              <div className="Date-Allegend">
+                <label>Date of Allegend</label>
+                <input type="date"/>
+              </div>
+            </div>
+            <div className="Nature-col-2">
+                <div className="Reported1">
+                    <label>Date Reported / Initiated</label>
+                    <input type="date"/>
+                </div>
+                <div className="Reportaing-stage">
+                    <label>Duration of Reportaing Stage (in days)</label>
+                </div>
+                <div className="Reported2">
+                    <label>Reported
+                    <select>
+                    <option value="">Other</option>
+                    <option value="">Case1</option>
+                    <option value="">Case2</option>
+                    <option value="">Case3</option>
+                    <option value="">Case4</option>
+                  </select>
+                    </label>
+                    
+                </div>
+                <div className="Complainanat">
+                    <label>Complainant Name</label>
+                    <input type="text"/>
+                </div>
+                <div className="Complainanat-Category">
+                  <label>Complainanat Category
+                  <select>
+                    <option value=""></option>
+                    <option value="">Category1</option>
+                    <option value="">Category2</option>
+                    <option value="">Category3</option>
+                    <option value="">Category4</option>
+                    <option value="">Category5</option>
+                  </select>
+                    </label> 
+
+                </div>
+                <div className="Manager">
+                <label>HR Case Manager
+                  <select>
+                    <option value=""></option>
+                    <option value="">Case1</option>
+                    <option value="">Case2</option>
+                    <option value="">Case3</option>
+                    <option value="">Case4</option>
+                    <option value="">Case5</option>
+                  </select>
+                    </label> 
+                </div>
+                <div className="Informal">
+                <label>Informal Case Action
+                  <select>
+                    <option value=""></option>
+                    <option value="">Case1</option>
+                    <option value="">Case2</option>
+                    <option value="">Case3</option>
+                    <option value="">Case4</option>
+                    <option value="">Case5</option>
+                  </select>
+                    </label> 
+                </div>
+            </div>
           </div>
         </div>
       </div>
